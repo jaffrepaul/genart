427f328ccf06e1c1403887cf11495221659e73f1
paulj <jaffrepaul@gmail.com>
2019-05-06 13:22:47 -0400

    wrapped grid in margin to keep within canvas element

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 const canvasSketch = require('canvas-sketch');
+/* lerp = linear interpolation -> give grid a margin using 
+min,max bounds & a num between min&max */
+const { lerp } = require('canvas-sketch-util/math');
 
 const settings = {
     dimensions: [2048, 2048]
@@ -11,8 +14,8 @@ const sketch = () => {
 
         for (let x = 0; x < count; x++) {
             for (let y = 0; y < count; y++) {
-                const u = count <= 1 ? 0.5 : x / (count - 1); 
-                const v = count <= 1 ? 0.5 : y / (count - 1); 
+                const u = count <= 1 ? 0.5 : x / (count - 1);
+                const v = count <= 1 ? 0.5 : y / (count - 1);
                 points.push([u, v]);
             }
         }
@@ -20,17 +23,18 @@ const sketch = () => {
     };
 
     const points = createGrid();
+    const margin = 400;
 
     return ({ context, width, height }) => {
         context.fillStyle = 'white';
         context.fillRect(0, 0, width, height);
 
         points.forEach(([u, v]) => {
-            const x = u * width;
-            const y = v * height;
+            const x = lerp(margin, width - margin, u);
+            const y = lerp(margin, width - margin, v);
 
             context.beginPath();
-            context.arc(x, y, 200, 0, Math.PI * 2, false);
+            context.arc(x, y, 100, 0, Math.PI * 2, false);
             context.strokeStyle = 'black';
             context.lineWidth = 40;
 
